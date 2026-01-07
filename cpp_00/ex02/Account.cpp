7a850b6a6d284499916b1b6d707ab95b936d130a
Keith <keith.teo2005@gmail.com>
2026-01-07 15:06:10 +0800

    implemented destructor and constructor functions

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -14,6 +14,37 @@
 #include <ctime>
 #include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex 
+			  << ";amount:" << _amount 
+			  << ";created" 
+			  << std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex 
+			  << ";amount:" << _amount 
+			  << ";closed" 
+			  << std::endl;
+	_nbAccounts--;
+}
+
 int Account::getNbAccounts()
 {
 	return _nbAccounts;
